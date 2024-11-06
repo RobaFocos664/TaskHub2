304b40bb53d302bae7472f4f39d23afb3f151c1c
RobaFocos664 <0323105957@ut-tijuana.edu.mx>
2024-11-05 16:19:34 -0800

    Update index.js

--- AvanT2.3/jsP/index.js
+++ AvanT2.3/jsP/index.js
@@ -1,1 +1,11 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.image-slider img');
+const totalImages = images.length;
 
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % totalImages;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 3300); // Cambia de imagen cada 3 segundos
